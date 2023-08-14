2d33a9ebc126219d3d4c58834b6b39ecfe1b584c
Vladyyde <noha.official@hotmail.com>
2023-08-14 17:36:17 +0200

    generaor1

--- Lesson9/Generator/main.py
+++ Lesson9/Generator/main.py
@@ -1,11 +1,14 @@
 def generate_grid(N):
     if N <= 0:
-        return "0"
+        yield "0"
 
     grid = [["#" for _ in range(N)] for _ in range(N)]
 
     for row in grid:
-        print("".join(row))
+        yield "".join(row)
 
 N = 4
-generate_grid(N)
+grid_generator = generate_grid(N)
+
+for row in grid_generator:
+    print(row)
